cc16f2b90ee1bdb2248a4c2a44fd417e307ecc99
maxwen <max.weninger@gmail.com>
2014-06-06 00:06:28 +0200

    TextView: Check also for mLayoutParams before calling checkForRelayout
    Cherry-picked from OmniROM.
    
    http://jira.omnirom.org/browse/OMNI-976
    
    This was introduced with
    https://github.com/omnirom/android_frameworks_base/commit/b3670c54e60387fde633fb91c649c5b896f8b962
    
    Change-Id: I06c1926c5b8102b336d128d9b411f74a9eff3ded

--- core/java/android/widget/TextView.java
+++ core/java/android/widget/TextView.java
@@ -4043,7 +4043,7 @@ public class TextView extends View implements ViewTreeObserver.OnPreDrawListener
             }
         }
 
-        if (mLayout != null) {
+        if (mLayout != null && mLayoutParams != null) {
             checkForRelayout();
         }
 
@@ -4137,7 +4137,7 @@ public class TextView extends View implements ViewTreeObserver.OnPreDrawListener
     public final void setHint(CharSequence hint) {
         mHint = TextUtils.stringOrSpannedString(hint);
 
-        if (mLayout != null) {
+        if (mLayout != null && mLayoutParams != null) {
             checkForRelayout();
         }
 
