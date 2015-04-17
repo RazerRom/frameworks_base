CyanogenMod/android_frameworks_base

Add static patternToString() with pattern size
Sometimes we need to generate a pattern string when lock pattern utils
cannot query the actual size of the pattern. Add a method which doesn't
depend on LockPatternUtils and accepts a pattern length parameter to
generate the string.

Change-Id: If73334b7030800bed2eec0cd0934b762b10dcaa3
Signed-off-by: Roman Birg <roman@cyngn.com>

core/java/com/android/internal/widget/LockPatternUtils.java
@@ -993,15 +993,24 @@ public class LockPatternUtils {
      * @return The pattern in string form.
      */
     public String patternToString(List<LockPatternView.Cell> pattern) {
+        return patternToString(pattern, getLockPatternSize());
+    }
+
+    /**
+     * Serialize a pattern.
+     * @param pattern The pattern.
+     * @param patternSize the pattern size
+     * @return The pattern in string form.
+     */
+    public static String patternToString(List<LockPatternView.Cell> pattern, int patternSize) {
         if (pattern == null) {
             return "";
         }
-        final int patternSize = pattern.size();
 
         byte[] res = new byte[patternSize];
         for (int i = 0; i < patternSize; i++) {
             LockPatternView.Cell cell = pattern.get(i);
-            res[i] = (byte) (cell.getRow() * getLockPatternSize() + cell.getColumn());
+            res[i] = (byte) (cell.getRow() * patternSize + cell.getColumn());
         }
         return new String(res);
     }

RazerRom/frameworks_base

Change-Id: I92f44c433fc8f84d7aca54916c48852e1e21ea38

core/java/com/android/internal/widget/LockPatternUtils.java
@@ -1029,6 +1029,16 @@ public class LockPatternUtils {
      * @return The pattern in string form.
      */
     public String patternToString(List<LockPatternView.Cell> pattern) {
+        return patternToString(pattern, getLockPatternSize());
+    }
+
+    /**
+     * Serialize a pattern.
+     * @param pattern The pattern.
+     * @param patternGridSize the pattern size
+     * @return The pattern in string form.
+     */
+    public static String patternToString(List<LockPatternView.Cell> pattern, int patternGridSize) {
         if (pattern == null) {
             return "";
         }
@@ -1037,7 +1047,7 @@ public class LockPatternUtils {
         byte[] res = new byte[patternSize];
         for (int i = 0; i < patternSize; i++) {
             LockPatternView.Cell cell = pattern.get(i);
-            res[i] = (byte) (cell.getRow() * getLockPatternSize() + cell.getColumn());
+            res[i] = (byte) (cell.getRow() * patternGridSize + cell.getColumn());
         }
         return new String(res);
     }