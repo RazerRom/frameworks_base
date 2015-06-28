f3487f99c8b7a873d2c31b12e8afab56a6774b42
tobitege <tobiasteschner@googlemail.com>
2015-06-28 10:46:09 +0200

    Revert "Show carrier label / custom & change color [1/2]"
    This reverts commit fdfe57777141dce59bfd6c52f366a7d97c79a1d3 and
    removes those features from both the normal and the MSIM
    network controllers accordingly.
    
    This commits the essential parts of the original commit in
    preparation of mergin CM's signal cluster commit.
    
    The original commit: fdfe57777141dce59bfd6c52f366a7d97c79a1d3
    
    Change-Id: Iaf52ede125e0e0f62339f408e9bb51d49283eeb4
    Signed-off-by: gamerman123x <19superguy98@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/widget/CarrierLabel.java

--- core/java/com/android/internal/util/razer/razerUtils.java
+++ core/java/com/android/internal/util/razer/razerUtils.java
@@ -17,9 +17,6 @@
 package com.android.internal.util.razer;
 
 import android.content.Context;
-import android.content.res.Resources;
-
-import java.util.Locale;
 
 public class razerUtils {
 
@@ -27,10 +24,4 @@ public class razerUtils {
         return context.getResources().getBoolean(
                 com.android.internal.R.bool.config_showNavigationBar);
     }
-
-    public static boolean isChineseLanguage() {
-       return Resources.getSystem().getConfiguration().locale.getLanguage().startsWith(
-               Locale.CHINESE.getLanguage());
-    }
-
 }
