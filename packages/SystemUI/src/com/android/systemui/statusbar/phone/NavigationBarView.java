cd6564ce9f90a26335fe5ebf175e9b6f3d2d88ce
Daniel Koman <dankoman30@gmail.com>
2015-07-17 15:46:50 +0200

    SlimDim: listen for touches in navbar empty areas also
    Change-Id: I9689df514bc0555e6fde8bd372a2e5981b42417d

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -260,6 +260,15 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
         }
     };
 
+    // provides a listener for the empty space in the navbar
+    private final OnTouchListener mNavButtonsTouchListener = new OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+                onNavButtonTouched();
+            return true;
+        }
+    };
+
     public void onNavButtonTouched() {
         if (mIsHandlerCallbackActive) {
             mHandler.removeCallbacks(mNavButtonDimmer);
@@ -1357,6 +1366,11 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
         if (getImeSwitchButton() != null)
             getImeSwitchButton().setOnClickListener(mImeSwitcherClickListener);
 
+        if (mDimNavButtons) {
+            if (getNavButtons() != null)
+                getNavButtons().setOnTouchListener(mNavButtonsTouchListener);
+        }
+
         updateRTLOrder();
     }
 
@@ -1387,6 +1401,11 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
 
         if (getImeSwitchButton() != null)
             getImeSwitchButton().setOnClickListener(mImeSwitcherClickListener);
+
+        if (mDimNavButtons) {
+            if (getNavButtons() != null)
+                getNavButtons().setOnTouchListener(mNavButtonsTouchListener);
+        }
 
         mDeadZone = (DeadZone) mCurrentView.findViewById(R.id.deadzone);
         mDeadZone.setStartFromRight(mLeftInLandscape);
