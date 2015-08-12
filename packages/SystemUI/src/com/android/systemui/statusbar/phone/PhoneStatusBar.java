5b5f683994abe75bca2916b53390a726ad659217
Roman Birg <roman@cyngn.com>
2015-08-12 12:56:31 -0700

    SystemUI: fix empty notification space in shade_locked
    In 8e98622a2c514f8bd11743781d4097e544d2f580, we started
    funneling update notification calls through the handler
    to avoid queueing up hundreds of these calls if a
    notification became spammy and to only execute one update if many were
    queued.
    
    However, some animations and state transitions are expecting the action
    to be done immediately after the call. Facilitate these requests by
    allowing them to bypass the handler if they want.
    
    Change-Id: Ice14cdaf93a4d370779b4a3e8996d17cd42fdff8
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2511,9 +2511,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     @Override
-    protected void updateNotifications() {
-        if (!mHandler.hasMessages(MSG_UPDATE_NOTIFICATIONS)) {
-            mHandler.sendEmptyMessage(MSG_UPDATE_NOTIFICATIONS);
+    protected void updateNotifications(boolean immediate) {
+        if (immediate) {
+            mHandler.removeMessages(MSG_UPDATE_NOTIFICATIONS);
+            handleUpdateNotifications();
+        } else {
+            if (!mHandler.hasMessages(MSG_UPDATE_NOTIFICATIONS)) {
+                mHandler.sendEmptyMessage(MSG_UPDATE_NOTIFICATIONS);
+            }
         }
     }
 
@@ -4581,7 +4586,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (MULTIUSER_DEBUG) mNotificationPanelDebugText.setText("USER " + newUserId);
         animateCollapsePanels();
         updatePublicMode(); 
-        updateNotifications();
+        updateNotifications(true);
         resetUserSetupObserver();
         setControllerUsers();
 
@@ -5395,7 +5400,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateDozingState();
         updatePublicMode();
         updateStackScrollerState(goingToFullShade);
-        updateNotifications();
+        updateNotifications(true);
         checkBarModes();
         updateCarrierLabelVisibility(false);
         updateMediaMetaData(false);
